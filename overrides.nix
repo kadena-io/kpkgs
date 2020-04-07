b98e4e1f8215e8b2ab650a91b719049bcb4280b1
Colin Woodbury <colin@fosskers.ca>
2020-04-07 16:32:32 -0700

    Up-pin sbv

--- overrides.nix
+++ overrides.nix
@@ -105,10 +105,9 @@ in with pkgs.haskell.lib; {
 
   sbv = dontCheck (callHackageDirect {
     pkg = "sbv";
-    ver = "8.2";
-    sha256 = "1isa8p9dnahkljwj0kz10119dwiycf11jvzdc934lnjv1spxkc9k";
+    ver = "8.6";
+    sha256 = "1zza7kihmmjxg476rkyaakfvlj1r0hn2z0wp51432pmf36pjz07p";
   });
-
 
   semialign = callHackageDirect {
     pkg = "semialign";
